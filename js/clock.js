6aa3bc3cebb939aa0a4aee07b4133c5f737f2fa0
Tim Krofecheck <tkrofecheck@webmd.net>
2017-03-09 17:04:21 -0500

    error handling

--- js/clock.js
+++ js/clock.js
@@ -17,6 +17,10 @@ wp.alarmClock = {
 			name: 'second',
 			min: 0,
 			max: 59
+		},
+		meridiem: {
+			min: 'AM',
+			max: 'PM'
 		}
 	},
 
@@ -39,31 +43,13 @@ wp.alarmClock = {
 		_this.createAlarmDropDown(_this.config.hour);
 		_this.createAlarmDropDown(_this.config.minute);
 		_this.createAlarmDropDown(_this.config.second);
+		_this.createAlarmDropDown(_this.config.meridiem);
 		_this.bindEvents();
-	},
-
-	bindEvents: function() {
-		var _this = this;
-
-		$('input select').change(function(evt) {
-			console.log('evt', evt);
-			
-			if (evt.currentTarget.name === 'alarm') {
-				if (this.value === 'off') {
-					_this.unsetAlarm();
-				} else {
-					_this.setAlarm();
-				}
-			}
-
-			return true;
-		});
 	},
 
 	setClock: function() {
     	var _this = this,
     		time = new Date(), // get date from client computer
-    		meridiem = 'AM',
     		doubleDigit = function(i) {
     			// Check if value is less than 10
     			if (i < 10) {
@@ -77,11 +63,12 @@ wp.alarmClock = {
 			formatHour = function(h) {
 				var hh = h;
 
+				_this.meridiem = "AM";
 				console.log('hour', h);
 
 				if (h >= 12) {
 					h = hh-12;
-					meridiem = "PM";
+					_this.meridiem = "PM";
 				}
 
 				if (h === 0) {
@@ -106,7 +93,7 @@ wp.alarmClock = {
 		_this.min = doubleDigit(_this.min);
     	_this.sec = doubleDigit(_this.sec);
 
-    	document.getElementById('clock').innerHTML = formatHour(_this.hour) + ':' + _this.min + ':' + _this.sec + " " + meridiem;
+    	document.getElementById('clock').innerHTML = formatHour(_this.hour) + ':' + _this.min + ':' + _this.sec + " " + _this.meridiem;
 
     	// Continue if stopClock is not set
     	// Use setTimeout to allow clock to refresh as seconds count up
@@ -126,19 +113,59 @@ wp.alarmClock = {
 		$('#alarm').append($('<select name="' + obj.name + '" id="' + obj.name + '"/>'));
 		$select = $('select[name="' + obj.name + '"]');
 
-		$select.append($('<option label="' + obj.name + '"/>'));
+		if (typeof obj.name !== 'undefined' && obj.name !== null && obj.name !== '') {
+			$select.append($('<option label="' + obj.name + '"/>'));
+		}
 		
-		for (i=obj.min; i<=obj.max; i++) {
-			$option = $('<option value="' + i + '">' + i + '</option>');
+		if (!isNaN(obj.min) || !isNaN(obj.max)) {
+			for (i=obj.min; i<=obj.max; i++) {
+				$option = $('<option value="' + i + '">' + i + '</option>');
+				$select.append($option);
+			}
+		} else {
+			$option = $('<option value="' + obj.min + '">' + obj.min + '</option>');
+			$select.append($option);
+			$option = $('<option value="' + obj.max + '">' + obj.max + '</option>');
 			$select.append($option);
 		}
 	},
 
 	setAlarm: function() {
-		var _this = this;
+		var _this = this,
+			hour = $('select[name="hour"]').val(),
+			min = $('select[name="minute"]').val(),
+			sec = $('select[name="second"]').val(),
+			msg;
 
-		_this.config.alarmOn = true;
-		console.log('setAlarm');
+		if (noErr()) {
+			console.log('set hour', hour);
+
+			_this.config.alarmOn = true;
+			_this.alarmTime = new Date(hour + ":" + min + ":" + sec + " " + meridian);
+			console.log('setAlarm');
+		} else {
+			alert(msg);
+		}
+
+		function noErr() {
+			if (isNaN(hour)) {
+				msg = 'Please select an hour\n';
+			}
+
+			if (isNaN(min)) {
+				msg = 'Please select an minute\n';
+			}
+
+			if (isNaN(sec)) {
+				msg = 'Please select a second\n';
+			}
+
+			if (msg.length > 1) {
+				return false;
+			}
+
+			return true;
+		}
 	},
 
 	unsetAlarm: function() {
@@ -158,6 +185,28 @@ wp.alarmClock = {
 		var _this = this;
 
 		_this.stop = true;
+	},
+
+	bindEvents: function() {
+		var _this = this;
+
+		$('input, select').change(function(evt) {
+			console.log('evt', evt);
+			
+			if (evt.currentTarget.name === 'alarm') {
+				if (this.value === 'off') {
+					_this.unsetAlarm();
+				} else {
+					_this.setAlarm();
+				}
+			} else {
+				if (_this.config.alarmOn) {
+					_this.setAlarm();
+				}
+			}
+
+			return true;
+		});
 	}
 };
 
